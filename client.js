c9e6e05ae88d1bff328c9b42d0369a51b73b3f2b
Antony <ac.chan@live.com>
2021-06-30 22:56:36 +0000

    movement

--- client.js
+++ client.js
@@ -7,9 +7,31 @@ const connect = function() {
   });
   conn.setEncoding("utf8");
 
-  conn.on("connect", (client) => {
+  conn.on("connect", () => {
     console.log("Snake is online");
     conn.write("Name: Pog");
+    setInterval(() => {
+      conn.write("Move: up");
+    }, 50);
+    setInterval(() => {
+      conn.write("Move: left");
+    }, 75);
+    setInterval(() => {
+      conn.write("Move: down");
+    }, 65);
+
+
+
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: right");
+    // conn.write("Move: right");
+    // conn.write("Move: down");
+    // conn.write("Move: down");
+    // conn.write("Move: left");
+    // conn.write("Move: left");
+
+
   });
 
  conn.on("data", (data) => {
