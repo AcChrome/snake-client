f356f3d8849986b762330c72d37d893fc4d44945
Antony <ac.chan@live.com>
2021-06-30 21:07:27 +0000

    play update

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "135.23.223.133",
+    port: 50542
+  });
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("test");
+  });
+
+ conn.on("data", (data) => {
+    console.log('server says', data);
+  });
+
+
+
+
+  return conn;
+};
+
+
+console.log("Connecting...");
+connect();
