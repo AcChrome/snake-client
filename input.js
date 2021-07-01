42a260ad96854c6aabd55b3c21e0d866bdcb6155
Antony <ac.chan@live.com>
2021-07-01 02:24:51 +0000

    done

--- input.js
+++ input.js
@@ -1,20 +1,27 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on( conn, handleUserInput);
 
 
   return stdin;
 };
 
 const handleUserInput = function (key) {
+
   if (key === '\u0003') {
     process.exit();
   }
   if(key === 'w') {
     conn.write("Move: up");
+  }
+  if(key === 'a') {
+    conn.write("Move: left");
   }
   if(key === 's') {
     conn.write("Move: down");
@@ -22,9 +29,10 @@ const handleUserInput = function (key) {
   if(key === 'd') {
     conn.write("Move: right");
   }
-  if(key === 'a') {
-    conn.write("Move: left");
+  if(key === 'z') {
+    conn.write("Say: LETS GO");
   }
+
 
 };
 
